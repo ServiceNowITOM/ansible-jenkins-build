7ceab9112acfa8b213f18dd636569285261ed934
Ubuntu <ubuntu@ip-172-31-39-71.ec2.internal>
2016-06-06 19:26:42 +0000

    fixed typo in YAML and added Jenkins role

--- build-ci-server.sh
+++ build-ci-server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get -y install git
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+cd ~
+git clone https://github.com/amittell/ansible-jenkins-build
 export _JAVA_OPTIONS="-Djava.net.prefer.IPv4Stack=true"
 /usr/bin/ansible-playbook configure-ci-server.yml --extra-vars "variable_host=localhost" -i "localhost," --connection=local
